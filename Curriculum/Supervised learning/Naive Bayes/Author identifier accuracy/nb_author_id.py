b624838eb79eebd926693a290d527e4b0a4429e1
Zhicheng Cai <yzzsjc1234@gmail.com>
2017-07-27 18:37:02 +0800

    Added GaussianNB usage & accuracy and updated author identifier t&p time

--- Curriculum/Supervised learning/Naive Bayes/Author identifier accuracy/nb_author_id.py
+++ Curriculum/Supervised learning/Naive Bayes/Author identifier accuracy/nb_author_id.py
@@ -28,8 +28,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.naive_bayes import MultinomialNB
 clf = MultinomialNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print ("training time:",round(time()-t0,3),"s")
+t0 = time()
 pred = clf.predict(features_test)
+print ("predict time:",round(time()-t0,3),"s")
 from sklearn.metrics import accuracy_score
 print (accuracy_score(pred, labels_test))
 
