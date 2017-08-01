3b4ba617bb7b2f282df035e437e28af3585ef017
Zhicheng Cai <yzzsjc1234@gmail.com>
2017-08-02 01:48:31 +0800

    Added Outliers removal mini project

---
+++ Curriculum/Supervised learning/Regression/Outliers removal mini project/outlier_cleaner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+
+def outlierCleaner(predictions, ages, net_worths):
+    """
+        Clean away the 10% of points that have the largest
+        residual errors (difference between the prediction
+        and the actual net worth).
+
+        Return a list of tuples named cleaned_data where 
+        each tuple is of the form (age, net_worth, error).
+    """
+    
+    cleaned_data = []
+    error = []
+    ### your code goes here
+    for i in range(len(predictions)):
+        error.append(abs(net_worths[i][0]-predictions[i][0]))
+    cleaned_data = list(zip(ages,net_worths,error))
+    cleaned_data = sorted(cleaned_data, key=lambda x:x[2])
+    cleaned_data = cleaned_data[:int(len(cleaned_data)*0.9)]
+    
+    return cleaned_data
+
